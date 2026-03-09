18111452b741146624e70b5d402eac33aae108e5
faith16437 <faith16437@gmail.com>
2026-03-09 08:59:10 -0700

    deposit-controller.js

--- deposit-controller.js
+++ deposit-controller.js
@@ -1,78 +1,57 @@
 (async function () {
 
-  // read selected coin
-  const params = new URLSearchParams(window.location.search);
-  let urlCoin = params.get("a");
+const params = new URLSearchParams(window.location.search);
+let symbol = params.get("a");
 
-  let storedCoin = null;
+if (!symbol) symbol = "btc";
 
-  try {
-    storedCoin = JSON.parse(localStorage.getItem("SELECTED_COIN"));
-  } catch {
-    storedCoin = null;
-  }
+symbol = symbol.split("-")[0].toUpperCase();
 
-  let symbol =
-    (urlCoin ||
-      storedCoin?.symbol ||
-      "BTC"
-    ).split("-")[0].toUpperCase();
+const res = await fetch("/wallets.json");
+const data = await res.json();
 
+let address = data.wallets[symbol];
 
-  // load wallets
-  const res = await fetch("/wallets.json");
-  const data = await res.json();
+const ERC20 = ["SHIB","LINK","PEPE","ONDO","QNT","LCX","USDT","BNB"];
+const TRC20 = ["USDT_TRC20"];
 
-  let address = data.wallets[symbol];
+if (ERC20.includes(symbol)) {
+address = data.wallets["ETH"];
+}
 
+if (symbol === "USDT_TRC20") {
+address = data.wallets["TRX"];
+}
 
-  // network mappings
+const coinEl = document.getElementById("depositCoin");
+const addrEl = document.getElementById("depositAddress");
+const qrEl = document.getElementById("depositQR");
+const copyBtn = document.getElementById("copyAddress");
 
-  const ERC20 = ["SHIB","LINK","PEPE","ONDO","QNT","LCX","USDT","BNB"];
-  const TRC20 = ["USDT_TRC20"];
+if (coinEl) coinEl.textContent = symbol;
 
-  if (ERC20.includes(symbol)) {
-    address = data.wallets["ETH"];
-  }
+if (addrEl) addrEl.textContent = address;
 
-  if (symbol === "USDT_TRC20") {
-    address = data.wallets["TRX"];
-  }
+if (qrEl && address) {
+qrEl.src =
+"https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" +
+encodeURIComponent(address);
+}
 
+if (copyBtn && address) {
 
-  // update page
+copyBtn.addEventListener("click", () => {
 
-  const coinEl = document.getElementById("depositCoin");
-  const addrEl = document.getElementById("depositAddress");
-  const qrEl = document.getElementById("depositQR");
+navigator.clipboard.writeText(address);
 
-  if (coinEl) coinEl.textContent = symbol;
+copyBtn.innerText = "Copied!";
 
-  if (addrEl) addrEl.textContent = address;
+setTimeout(() => {
+copyBtn.innerText = "Copy";
+}, 2000);
 
+});
 
-  // generate QR code
-
-  if (qrEl && address) {
-    qrEl.src =
-      "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=" +
-      encodeURIComponent(address);
-  }
-
-
-  // copy address button
-
-  const copyBtn = document.getElementById("copyAddress");
-
-  if (copyBtn && address) {
-    copyBtn.addEventListener("click", () => {
-      navigator.clipboard.writeText(address);
-
-      copyBtn.innerText = "Copied!";
-      setTimeout(() => {
-        copyBtn.innerText = "Copy";
-      }, 2000);
-    });
-  }
+}
 
 })();
